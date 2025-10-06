3c383fe906b1e0c6021816a372d1c7561b77ebf9
Winorrepeat <155412365+ShawrmaM8@users.noreply.github.com>
2025-10-06 19:20:01 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+// src/main.tsx
+import { ClerkProvider } from '@clerk/clerk-react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ClerkProvider publishableKey={process.env.CLERK_PUBLISHABLE_KEY!}>
+    <App />
+  </ClerkProvider>
+);
